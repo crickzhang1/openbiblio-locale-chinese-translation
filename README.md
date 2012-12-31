eccffad26271ddd516af35887eecc22f6f2b5bd5
Yinhe Zhang <crickzhang1@gmail.com>
2012-12-31 22:30:01 +0800

    Completed translation of zh_CN/home.php
    Add a section about translation progress.

--- README.md
+++ README.md
@@ -10,6 +10,51 @@ the latest release (0.7.1) of OpenBiblio.
 
 * Currently only translation for simplified Chinese is ongoing.
 
+### Progress of translation
+
+Files under `zh_CN`:
+
+* `zh_CN/admin.php`: __DONE__
+* `zh_CN/cataloging.php`: TODO
+* `zh_CN/circulation.php`: TODO
+* `zh_CN/classes.php`: TODO
+* `zh_CN/home.php`: __DONE__
+* `zh_CN/metadata.php`: TODO
+* `zh_CN/navbars.php`: TODO
+* `zh_CN/opac.php`: TODO
+* `zh_CN/reports.php`: TODO
+* `zh_CN/shared.php`: TODO
+* `zh_CN/template.php`:TODO
+* `zh_CN/help/barcodes.php`: TODO
+* `zh_CN/help/biblioCopyEdit.php``: TODO
+* `zh_CN/help/biblioEdit.php`: TODO
+* `zh_CN/help/biblioSearch.php`: TODO
+* `zh_CN/help/checkin.php`: TODO
+* `zh_CN/help/circulation.php`: TODO
+* `zh_CN/help/contents.php`: TODO
+* `zh_CN/help/memberView.php`: TODO
+* `zh_CN/help/opacLookup.php`: TODO
+* `zh_CN/help/opac.php`: TODO
+* `zh_CN/help/status.php`: TODO
+* `zh_CN/sql/0.7.1/domain/biblio_status_dm.sql`: TODO
+* `zh_CN/sql/0.7.1/domain/checkout_privs.sql`: TODO
+* `zh_CN/sql/0.7.1/domain/collection_dm.sql`: TODO
+* `zh_CN/sql/0.7.1/domain/material_type_dm.sql`: TODO
+* `zh_CN/sql/0.7.1/domain/mbr_classify_dm.sql`: TODO
+* `zh_CN/sql/0.7.1/domain/member_fields_dm.sql`: TODO
+* `zh_CN/sql/0.7.1/domain/settings.sql`: TODO
+* `zh_CN/sql/0.7.1/domain/staff.sql`: TODO
+* `zh_CN/sql/0.7.1/domain/theme.sql`: TODO
+* `zh_CN/sql/0.7.1/domain/transaction_type_dm.sql`: TODO
+* `zh_CN/sql/0.7.1/domain/usmarc_block_dm.sql`: TODO
+* `zh_CN/sql/0.7.1/domain/usmarc_indicator_dm.sql`: TODO
+* `zh_CN/sql/0.7.1/domain/usmarc_subfield_dm.sql`: TODO
+* `zh_CN/sql/0.7.1/domain/usmarc_tag_dm.sql`: TODO
+* `zh_CN/sql/0.7.1/sample/biblio_copy.sql`: TODO
+* `zh_CN/sql/0.7.1/sample/biblio.sql`: TODO
+* `zh_CN/sql/0.7.1/sample/member_fields.sql`: TODO
+* `zh_CN/sql/0.7.1/sample/member.sql`: TODO
+
 ## Relative Sites
 
 * OpenBiblio official: http://obiblio.sourceforge.net/
--- zh_CN/home.php
+++ zh_CN/home.php
@@ -38,32 +38,31 @@
 #****************************************************************************
 #*  Translation text for page index.php
 #****************************************************************************
-$trans["indexHeading"]       = "\$text='Welcome to OpenBiblio';";
+$trans["indexHeading"]       = "\$text='欢迎来到OpenBiblio图书馆';";
 $trans["indexIntro"]         = "\$text=
-  'Use the navigation tabs at the top of each page to access the following library
-  administration sections.';";
-$trans["indexTab"]           = "\$text='Tab';";
-$trans["indexDesc"]          = "\$text='Description';";
-$trans["indexCirc"]          = "\$text='Circulation';";
-$trans["indexCircDesc1"]     = "\$text='Use this tab to manage your member records.';";
-$trans["indexCircDesc2"]     = "\$text='Member administration (new, search, edit, delete)';";
-$trans["indexCircDesc3"]     = "\$text='Member bibliography checkout, holds, account, and history';";
-$trans["indexCircDesc4"]     = "\$text='Bibliography checkin and shelving cart list';";
-//$trans["indexCircDesc5"]     = "\$text='Member late fee payment';";
-$trans["indexCat"]           = "\$text='Cataloging';";
-$trans["indexCatDesc1"]      = "\$text='Use this tab to manage your bibliography records.';";
-$trans["indexCatDesc2"]      = "\$text='Bibliography administration (new, search, edit, delete)';";
-//$trans["indexCatDesc3"]      = "\$text='Import bibliography from USMarc record';";
-$trans["indexAdmin"]         = "\$text='Admin';";
-$trans["indexAdminDesc1"]    = "\$text='Use this tab to manage staff and administrative records.';";
-$trans["indexAdminDesc2"]    = "\$text='Staff administration (new, edit, password, delete)';";
-$trans["indexAdminDesc3"]    = "\$text='General library settings';";
-$trans["indexAdminDesc5"]    = "\$text='Library material type list';";
-$trans["indexAdminDesc4"]    = "\$text='Library collection list';";
-$trans["indexAdminDesc6"]    = "\$text='Library theme editor';";
-$trans["indexReports"]       = "\$text='Reports';";
-$trans["indexReportsDesc1"]  = "\$text='Use this tab to run reports on your library data.';";
-$trans["indexReportsDesc2"]  = "\$text='Report.';";
-$trans["indexReportsDesc3"]  = "\$text='Labels.';";
+  '利用每页顶部的导航标签访问图书馆以下各功能区。';";
+$trans["indexTab"]           = "\$text='导航标签';";
+$trans["indexDesc"]          = "\$text='说明';";
+$trans["indexCirc"]          = "\$text='流通';";
+$trans["indexCircDesc1"]     = "\$text='利用此标签管理会员记录。';";
+$trans["indexCircDesc2"]     = "\$text='会员管理（添加、搜索、编辑、删除）';";
+$trans["indexCircDesc3"]     = "\$text='会员书目借阅、当前借阅状态、账户和借阅历史）';";
+$trans["indexCircDesc4"]     = "\$text='还书和书架车列表';";
+//$trans["indexCircDesc5"]     = "\$text='会员超期罚款';";
+$trans["indexCat"]           = "\$text='编目';";
+$trans["indexCatDesc1"]      = "\$text='利用此标签管理书目。';";
+$trans["indexCatDesc2"]      = "\$text='著作管理（添加、搜索、编辑、删除）';";
+//$trans["indexCatDesc3"]      = "\$text='从USMarc记录中导入书目';";
+$trans["indexAdmin"]         = "\$text='管理';";
+$trans["indexAdminDesc1"]    = "\$text='利用此标签管理工作人员信息和事务记录。';";
+$trans["indexAdminDesc2"]    = "\$text='工作人员管理（添加、编辑、密码修改、删除）';";
+$trans["indexAdminDesc3"]    = "\$text='常规图书馆设定';";
+$trans["indexAdminDesc5"]    = "\$text='图书馆馆藏类型列表';";
+$trans["indexAdminDesc4"]    = "\$text='图书馆作品集列表';";
+$trans["indexAdminDesc6"]    = "\$text='图书馆主题编辑器';";
+$trans["indexReports"]       = "\$text='报告';";
+$trans["indexReportsDesc1"]  = "\$text='利用此标签生成关于图书馆各类数据统计的报告。';";
+$trans["indexReportsDesc2"]  = "\$text='报告';";
+$trans["indexReportsDesc3"]  = "\$text='标识签（Labels）';";
 
 ?>
